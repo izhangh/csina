ea00ecd33921043353472b33069779bf0a31c75c
liubang <it.liubang@qq.com>
2016-09-12 22:06:47 +0800

    add list datastructure

--- include/list.h
+++ include/list.h
@@ -1,16 +1,26 @@
 #ifndef LIST_H
 #define LIST_H
 
+#include <stdbool.h>
+#include <stdlib.h>
 
+typedef enum IteratonResult {
+	DONE, BREAK
+} IteratonResult;
 
+/* list node */
 typedef struct node {
 	struct node *next;
 	void *data;
+	size_t size;
 } ListNode;
 
+/* ListIterator interface */
+typedef bool (*ListIterator)(void *);
 
-ListNode *listConstructor();
-
-
+ListNode *listConstructor(void *, size_t, ListNode *);
+ListNode *listRevers(ListNode *);
+void listDel(listNode *);
+IteratonResult listForEach(listNode *, ListIterator);
 
 #endif
---
+++ src/list.c
@@ -0,0 +1,57 @@
+#include <string.h>
+
+#include "list.h"
+
+/* construct a list node */
+ListNode *listConstructor(void *value, size_t size, ListNode *next)
+{
+	ListNode *node = malloc(sizeof(ListNode));
+	node->next = next;
+	node->value = malloc(size);
+	node->size = size;
+
+	memcpy(node->value, value, size);
+
+	return node;
+}
+
+/* reverse a list */
+ListNode *listReverse(ListNode *node)
+{
+	ListNode *prev = NULL;
+	while(node) {
+		ListNode *next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	return prev;
+}
+
+void listDel(ListNode *node)
+{
+	if (!node)
+		return;
+	do {
+		ListNode *next = node->next;
+		free(node->value);
+		free(node);
+		node = next;
+	} while(node);
+}
+
+IterationResult listForEach(ListNode *node, ListIterator iteratior)
+{
+	if (!node)
+		return DONE;
+
+	bool res;
+	do {
+		res = iterator(node->value);
+		node = node->next;
+	} while(node && res);
+
+	return res ? DONE : BREAK;
+}
+
