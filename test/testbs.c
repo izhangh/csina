89cb86045f1ec7b17065ad9afefb5adb9df61b62
liubang <it.liubang@qq.com>
2016-09-13 11:31:35 +0800

    make test

--- test/testbs.c
+++ test/testbs.c
@@ -10,5 +10,11 @@ int main()
 	
 	printf("%s\n", bs);
 
+	bsLCat(&bs, " hhh");
+	printf("%s\n", bs);
+
+	char *bs1 = bsConcat(bsConstructor("hello"), bsConstructor("world"));
+	printf("%s\n", bs1);
+
 	return 0;
 }
