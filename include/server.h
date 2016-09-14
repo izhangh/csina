eb47c7ffda68a2847e44c947a234ad90335e30f6
liubang <it.liubang@qq.com>
2016-09-14 11:08:00 +0800

    add server

---
+++ include/server.h
@@ -0,0 +1,24 @@
+#ifndef SERVER_H
+#define SERVER_H
+
+#include "list.h"
+#include "request.h"
+#include "response.h"
+
+typedef struct Server {
+	unsigned int port;
+	ListNode *headers;
+} Server;
+
+
+/* interfaces */
+typedef Response *(*Handler)(Request *);
+typedef Response *(**HandlerP)(Request *);
+
+Server *serverConstructor(uint16_t);
+void serverDel(Server *);
+void serverAddHandler(Server *, Handler);
+void serverAddStaticHandler(Server *);
+void serverRun(Server *);
+
+#endif
